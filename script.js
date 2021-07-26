1ffc0a83bc5aece204dcd693342251b5636c5965
FacundoEstrada <facundoestrada1015@gmail.com>
2021-07-26 20:30:39 -0300

    Agrego funcionalidad a los botones

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ IDBRequest.addEventListener("upgradeneeded", ()=>{
     });
 });
 
+IDBRequest.addEventListener("success", ()=>{
+    readTweet();
+});
+
+
 const getIDBData = mode =>{
     const db = IDBRequest.result;
     const IDBTransaction = db.transaction("tweets",mode);
@@ -20,7 +25,6 @@ const getIDBData = mode =>{
 
     return objectStore;
 }
-
 
 const addTweet = tw => {
     const IDBData = getIDBData("readwrite");
@@ -31,10 +35,11 @@ const readTweet = ()=>{
     const IDBData = getIDBData("readonly");
     const cursor = IDBData.openCursor();
     const fragment = document.createDocumentFragment();
+    containerTweets.innerHTML = "";
     cursor.addEventListener("success",()=>{
         if (cursor.result){
-            let tw = tweetsHTML(cursor.result.key,cursor.result.value);
-            fragment.appendChild(tw);
+            let twAdd = tweetsHTML(cursor.result.key,cursor.result.value);
+            fragment.appendChild(twAdd);
             cursor.result.continue();
         }else{
             containerTweets.appendChild(fragment);
@@ -54,13 +59,21 @@ const deleteTweet = key => {
     IDBData.delete(key);
 }
 
-
-
 btnSubmit.addEventListener('click',()=>{
     let tw = textarea.value;
+
     if (tw.length > 0){
-        addTweet({tw});
-        readTweet();
+        if (document.querySelector(".enabled")){
+           if (confirm("Tienes elementos sin guardar, quieres continuar?")){
+                addTweet({tw});
+                readTweet();
+                textarea.value = "";
+           } 
+        }else{
+            addTweet({tw});
+            readTweet();
+            textarea.value = "";
+        }
     }
 });  
 
@@ -74,17 +87,38 @@ const tweetsHTML = (id,tweetTexto) => {
 
     container.classList.add("tweets");
     option.classList.add("option");
-    saveButton.classList.add("btn", "btn-save");
+    saveButton.classList.add("btn", "btn-save","disabled");
     deleteButton.classList.add("btn", "btn-delete");
 
     saveButton.textContent = "Guardar";
     deleteButton.textContent = "Eliminar";
-    h3.innerHTML = tweetTexto.tweet;
+    h3.innerHTML = tweetTexto.tw;
+    h3.setAttribute("contenteditable","true");
+    h3.setAttribute("spellcheck", "false");
 
     option.appendChild(saveButton);
     option.appendChild(deleteButton);
     container.appendChild(h3);
     container.appendChild(option);
+
+    h3.addEventListener("keyup", ()=>{
+        saveButton.classList.remove("disabled");
+        saveButton.classList.add("enabled");
+    })
+
+    saveButton.addEventListener("click", ()=>{
+        if (!saveButton.classList.contains("disabled")){
+            let tw = h3.textContent;
+            editTweet(id,{tw});
+            saveButton.classList.add("disabled");
+            saveButton.classList.remove("enabled");
+        }
+    })
+
+    deleteButton.addEventListener("click", ()=>{
+        deleteTweet(id);
+        containerTweets.removeChild(container);
+    })
 
     return container;
 }
